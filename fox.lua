8966ab1a5ecf8e3efa4bf90bf55ea12169802d5d
Fox <Maiconoliveirasilva2010@gmail.com>
2026-04-09 23:49:05 -0300

    fox update

--- fox.lua
+++ fox.lua
@@ -1,1 +1,7 @@
 
+
+ZTwO0mNnjUc6e#oyjqXPXWPS9R4qpL%PAcI^@M7p9%*)YH7$5b
+s%Zh!cD84K8W@7%r0K!iAxo#okzcUS&zC$(L4^edp1T48JqtyX
+J6bQXW)xhxVjrdYeSG9LTZ4Ae^TWN*@2KwX@zth8nP%bhwOR&!
+bVFDG*ZeGIUvdCv&reArFpj1Q)@0n(kyYUI7jQaw!5hr3*Bti4
+u&daHQ8PobMG&bmR@l^l5MWwu)ps2sRX!C4OQbDD6!zZYi##Cx
